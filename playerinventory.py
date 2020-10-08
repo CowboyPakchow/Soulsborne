60580c8ddfe131d48b25e2227b83d4bb9d1c0528
Michael Nguyen <michael.nguyen@rbe.sk.ca>
2020-10-08 16:38:25 +0000

    Added and updated playerinventory.py to add items in Hero's inventory.

--- playerinventory.py
+++ playerinventory.py
@@ -1,38 +1,40 @@
-inventory = {"Hero": { 'long sword': 
-        'damage': 10,
-        'description': 'A standard sword.',
-        'protection': 0,
-    },
-    'basic shield': {
-        'damage': 0,
-        'description': 'Shield for blocking.',
-        'protection': 10,
-    },
-    'fire spell': {
-        'damage': 10,
-        'description': 'Standard magic spell.',
-        'protection': 0,
-    },
-    'gold coins': {
-        'damage': 0,
-        'description': 'Currency for bartering.',
-        'protection': 0,
-    }
-}
+inventory = {"Hero": {"Long Sword": 
+                      {"damage": 10, 
+                      "protection": 0,
+                      "description": "A standard sword."},
+                      "Basic Shield": 
+                      {"damage": 0, 
+                      "protection": 10,
+                      "description": "A shield for blocking."},
+                      "Fire Spell":
+                      {"damage": 10, 
+                      "protection": 0,
+                      "description": "A standard magic spell."},
+                      "Gold Coins": 
+                      {"damage": 0, 
+                      "protection": 0,
+                      "description": "Currency for bartering."}}
+                      }
+
+
+
 def player_inventory(player, inventory):
     protection_items = []
     weapons = []
+    items = []
     for item in inventory[player]:
-        description = inventory[player][item][description]
-        damage = inventory[player][item][damage]
-        protection = inventory[player][item][protection]
-        print(f" {player}'s {item} - {description}")
-        print(f" {damage}: {damage}")
-        print(f" {protection}: {protection}")
+        damage = inventory[player][item]["damage"]
+        protection = inventory[player][item]["protection"]
+        description = inventory[player][item]["description"]
+        print(f"{player}'s {item} - {description}")
+        print(f"* Damage: {damage}")
+        print(f"* Protection: {protection}")
         if protection != 0 and damage == 0:
             protection_items.append(item)
-        elif damage != 0:
+        elif damage != 0 and protection == 0:
             weapons.append(item)
-        return(protection_items, weapons)
+        elif damage == 0 and protection == 0:
+            items.append(item)
+    return protection_items, weapons, items
 
-print(inventory)
+player_inventory("Hero", inventory)
